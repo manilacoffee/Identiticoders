8d71fb27a32601ac5ebe7d1b93b9e272290b8194
RJ <RJ@RJ-PC>
2019-03-12 18:14:58 +0800

    Forgot Department.java

---
+++ src/java/main/com/identiticoders/assignment/beans/Department.java
@@ -0,0 +1,26 @@
+package com.identiticoders.assignment.beans;
+
+public class Department {
+	private int id;
+	  private String name;
+	  
+	  public Department() {}
+	  
+	  public int getId() {
+	    return id;
+	  }
+	  
+	  public void setId(int id) { this.id = id; }
+	  
+	  public String getName() {
+	    return name;
+	  }
+	  
+	  public void setName(String name) { this.name = name; }
+	  
+
+	  public String toString()
+	  {
+	    return "User [id=" + id + ", name=" + name + "]";
+	  }
+}
